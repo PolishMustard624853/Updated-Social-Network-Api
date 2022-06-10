2f771f491a3cd45644edd4ce3a44755eb2cfd3c6
Connor Semb <polishmustard710@gmail.com>
2022-06-10 10:04:56 -0500

    Able to retrieve data in insomnia

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -52,8 +52,12 @@ createUser({
     body
 }, res) {
     User.create(body)
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => res.status(400).json(err));
+        .then(dbUserData => {
+            console.log(dbUserData)
+            return res.json(dbUserData)})
+        .catch(err => {
+            console.log(err)
+            res.status(400).json(err)});
 },
 
   //update user by id
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const thoughtRoutes = require('./thought-Routes');
 const userRoutes = require('./userRoutes');
 
-router.use('/Thought', thoughtRoutes);
-router.use('/students', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
